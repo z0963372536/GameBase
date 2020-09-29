05b59cbf1ac875809b9718f3342665e7beadc132
z0963372536 <z0963372536@gmail.com>
2020-09-29 14:26:14 +0800

    Update UserService

--- src/main/java/tw/twcoding/leolee/user/service/UsersService.java
+++ src/main/java/tw/twcoding/leolee/user/service/UsersService.java
@@ -10,9 +10,13 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tw.twcoding.leolee.user.dao.RankDao;
 import tw.twcoding.leolee.user.dao.UserDataDao;
+import tw.twcoding.leolee.user.dao.UserProfileDao;
 import tw.twcoding.leolee.user.dao.UsersInfoDao;
+import tw.twcoding.leolee.user.model.Rank;
 import tw.twcoding.leolee.user.model.UserData;
+import tw.twcoding.leolee.user.model.UserProfile;
 import tw.twcoding.leolee.user.model.UsersInfo;
 import tw.twcoding.leolee.util.MailSender;
 
@@ -24,20 +28,11 @@ public class UsersService {
 	@Autowired
 	private UsersInfoDao userInfoDao;
 	@Autowired
+	private UserProfileDao userprofileDao;
+	@Autowired
 	private MailSender mailSender;
-
-	public UserData checkAcctAndEmail(UserData userData) {
-		String acct = userData.getAccount();
-		String email = userData.getEmail();
-		UserData finUserData = null;
-		try {
-			finUserData = userDataDao.getByAcctAndEmail(acct, email);
-		} catch (Exception e) {
-			System.out.println("checkAcctAndEmail Exception: " + e.getMessage());
-			return finUserData;
-		}
-		return finUserData;
-	}
+	@Autowired
+	private RankDao rankDao;
 
 	public Map<String, Object> getByLogin(UserData userData) {
 		String acct = userData.getAccount();
@@ -60,6 +55,105 @@ public class UsersService {
 		return result;
 	}
 
+	public void saveUserProfile(UserProfile userProfile) {
+		try {
+			userprofileDao.saveUserProfile(userProfile);
+		} catch (Exception e) {
+			System.out.println("UsersService saveUserProfile Exception: " + e.getMessage());
+		}
+	}
+
+	public Integer getProfileIdByUserId(UserData userData) {
+		Integer userId = userData.getUserId();
+		Integer profileId = null;
+		try {
+			profileId = userprofileDao.getProfileIdByUserId(userId);
+		} catch (Exception e) {
+			System.out.println("UsersService getProfileIdByUserId Exception: " + e.getMessage());
+			return profileId;
+		}
+		return profileId;
+	}
+
+	public UserProfile getProfileByUserId(UserData userData) {
+		Integer userId = userData.getUserId();
+		UserProfile userProfile = null;
+		try {
+			userProfile = userprofileDao.getByUserId(userId);
+		} catch (Exception e) {
+			System.out.println("UsersService getProfileByUserId Exception: " + e.getMessage());
+			return userProfile;
+		}
+		return userProfile;
+	}
+
+	public List<Rank> getAllRank() {
+		return rankDao.getAllRanks();
+	}
+
+	public Rank getRankById(Integer rankId) {
+		Rank rank = null;
+		if (rankId != 0) {
+			try {
+				rank = rankDao.getByRankId(rankId);
+			} catch (Exception e) {
+				System.out.println("UsersService getRankById Exception: " + e.getMessage());
+				return rank;
+			}
+		}
+		return rank;
+	}
+
+	public boolean processForgetPwd(UserData userData) {
+		UserData resetUserData = null;
+		boolean status = false;
+		try {
+			resetUserData = resetPwdCheck(userData);
+			String pwdString = resetPwd(resetUserData);
+			if (!pwdString.isEmpty()) {
+				status = sendNewPwd(resetUserData, pwdString);
+			}
+		} catch (Exception e) {
+			System.out.println("UsersService processForgetPwd Exception: " + e.getMessage());
+			return status;
+		}
+		return status;
+	}
+
+	public List<UserData> getAllUserDatas(UserData userData) {
+		List<UserData> userDatas = null;
+		try {
+			if (userData.getRankId() == 4) {
+				userDatas = userDataDao.getAllUserData();
+			}
+		} catch (Exception e) {
+			System.out.println("UsersService getAllUserDatas Exception: " + e.getMessage());
+			return userDatas;
+		}
+		return userDatas;
+	}
+
+	public boolean checkAcct(String acct) {
+		boolean result = false;
+		UserData userData = null;
+		try {
+			if (!acct.isEmpty()) {
+				userData = userDataDao.checkAcct(acct);
+			}
+			if (userData != null) {
+				result = true;
+			}
+		} catch (Exception e) {
+			System.out.println("UsersService checkAcct Exception: " + e.getMessage());
+			return result;
+		}
+		return result;
+	}
+	
+	public void deleteUserData(UserData userData) {
+		//TODO 
+	}
+
 	private String getMD5Endocing(String message) {
 		final StringBuffer buffer = new StringBuffer();
 		try {
@@ -80,7 +174,47 @@ public class UsersService {
 		return buffer.toString();
 	}
 
-	private String genRandomPwd(int pwd_len) {
+	private UserData resetPwdCheck(UserData userData) {
+		String acct = userData.getAccount();
+		String email = userData.getEmail();
+		UserData findData = null;
+		try {
+			findData = userDataDao.getByAcctAndEmail(acct, email);
+		} catch (Exception e) {
+			System.out.println("UsersService resetPwdCheck Exception: " + e.getMessage());
+			return findData;
+		}
+		return findData;
+	}
+
+	private String resetPwd(UserData userData) {
+		String newPwd = "";
+		String encryptPwd = "";
+		try {
+			newPwd = genRandomPwd(8);
+			encryptPwd = getMD5Endocing(newPwd);
+			userData.setPassword(encryptPwd);
+			userDataDao.addUserData(userData);
+		} catch (Exception e) {
+			System.out.println("UsersService resetPwd Exception: " + e.getMessage());
+			return newPwd;
+		}
+		return newPwd;
+	}
+
+	private boolean sendNewPwd(UserData userData, String newPwd) {
+		boolean status = false;
+		String mailTo = userData.getEmail();
+		try {
+			status = mailSender.pwdMailSender(mailTo, newPwd);
+		} catch (Exception e) {
+			System.out.println("UsersService sendNewPwd Exception: " + e.getMessage());
+			return status;
+		}
+		return status;
+	}
+
+	private String genRandomPwd(Integer pwd_len) {
 		final int maxNum = 36;
 		int i;
 		int count = 0;
